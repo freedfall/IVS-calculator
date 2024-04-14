d538104553935ee416004e20f792cf67122ff36a
switos <atomol112@gmail.com>
2024-04-14 15:31:58 +0200

    Updated basic tests for expression parser

--- src/test_expression.py
+++ src/test_expression.py
@@ -11,23 +11,44 @@ class TestAnalyzer(unittest.TestCase):
 
     def setUp(self):
         self.analyzer = Analyzer()  # Create an instance of Analyzer before each test
-
+    
     def test_simple_expression(self):
-        token_stream = ["id", "+", "id", "$"]
-        self.assertIsNone(self.analyzer.analyze(token_stream))
+        token_stream = [
+            {'item_type': 'T', 'value': '112', 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '+', 'token_type': 'OP1'},
+            {'item_type': 'T', 'value': '12.2', 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '$', 'token_type': 'END'}
+        ]
+        self.assertTrue(self.analyzer.analyze(token_stream))
 
     def test_complex_expression(self):
-        token_stream = ["id", "+", "id", "*", "id", "-", "id", "$"]
-        self.assertIsNone(self.analyzer.analyze(token_stream))
+        token_stream = [
+            {'item_type': 'T', 'value': 12, 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '+', 'token_type': 'OP1'},
+            {'item_type': 'T', 'value': 5, 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '*', 'token_type': 'OP2'},
+            {'item_type': 'T', 'value': 30, 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '-', 'token_type': 'OP1'},
+            {'item_type': 'T', 'value': 7.8, 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '$', 'token_type': 'END'}
+        ]
+        self.assertTrue(self.analyzer.analyze(token_stream))
 
     def test_invalid_expression(self):
-        token_stream = ["id", "+", "*", "id", "$"]  # Invalid: consecutive operators
-        with self.assertRaises(ValueError):
-            self.analyzer.analyze(token_stream)
+        token_stream = [
+            {'item_type': 'T', 'value': 'id', 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '+', 'token_type': 'OP1'},
+            {'item_type': 'T', 'value': '*', 'token_type': 'OP2'},
+            {'item_type': 'T', 'value': 'id', 'token_type': 'ID'},
+            {'item_type': 'T', 'value': '$', 'token_type': 'END'}
+        ]
+        self.assertFalse(self.analyzer.analyze(token_stream))
 
     def test_empty_expression(self):
-        token_stream = ["$"]  # Just the end marker
-        self.assertIsNone(self.analyzer.analyze(token_stream))
+        token_stream = [
+            {'item_type': 'T', 'value': '$', 'token_type': 'END'}
+        ]
+        self.assertTrue(self.analyzer.analyze(token_stream))
 
 if __name__ == '__main__':
     unittest.main()
