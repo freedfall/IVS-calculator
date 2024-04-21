0507fb92b100b8865baba3f18ccf1556f5efa106
switos <atomol112@gmail.com>
2024-04-21 16:12:39 +0200

    par tests added

--- src/test_analyser.py
+++ src/test_analyser.py
@@ -14,9 +14,18 @@ class TestAnalyserclass(unittest.TestCase):
     def test_simple_expression(self):
         expr = '12+34- 28.8 * 100'
         self.assertEqual(-2834,(self.analyser.analyse(expr)))
+
     def test_single_expression(self):
         expr = '1.0'
         self.assertEqual(1.0,(self.analyser.analyse(expr)) )
+        
+    def test_par(self):
+        expr = '(12+2)*2'
+        self.assertEqual(28, (self.analyser.analyse(expr)))
+    
+    def test_single_par(self):
+        expr = "()"
+        self.assertEqual(False, (self.analyser.analyse(expr))) 
 
 if __name__ == '__main__':
     unittest.main()
